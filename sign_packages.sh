3696e01d653f10defee375dd3179c84cd56dd020
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-04-17 12:13:36 +0200

    Updated

--- sign_packages.sh
+++ sign_packages.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo $PRIVATE_GPG_KEY > private.key
+echo -e $PRIVATE_GPG_KEY > ~/private.key
 
 cat private.key | wc -l
 
-gpg --allow-secret-key-import --import private.key
+gpg --allow-secret-key-import --import ~/private.key
 
 wget https://grafanarel.s3.amazonaws.com/builds/grafana-2.0.0_beta3-1.x86_64.rpm
 
