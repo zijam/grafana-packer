c31a158ea143fa23f99b7c8d4c2ac3f176be424f
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-04-17 14:07:40 +0200

    signing rpms might work now

--- sign_packages.sh
+++ sign_packages.sh
@@ -4,8 +4,6 @@ git clone git@github.com:torkelo/private.git ~/private-repo
 
 gpg --allow-secret-key-import --import ~/private-repo/signing/private.key
 
-wget https://grafanarel.s3.amazonaws.com/builds/grafana-2.0.0_beta3-1.x86_64.rpm
-
 cp rpmmacros ~/.rpmmacros
 
-./sign_expect $GPG_KEY_PASSWORD *.rpm
+./sign_expect $GPG_KEY_PASSWORD dist/*.rpm
