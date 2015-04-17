d140408dd263dadfd8f361fa200cc39c9e80ecc6
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-04-17 11:11:49 +0200

    added rpm macros file

--- sign_packages.sh
+++ sign_packages.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 wget https://grafanarel.s3.amazonaws.com/builds/grafana-2.0.0_beta3-1.x86_64.rpm
+
+cp rpmmacros ~/.rpmmacros
+
 echo $PRIVATE_GPG_KEY > private.key
 gpg --allow-secret-key-import --import private.key
+
 ./sign_expect *.rpm
