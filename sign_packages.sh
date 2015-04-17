e308a075bd57727b072abd4c50b70fa3c3c7f8ef
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-04-17 11:21:47 +0200

    Updated

--- sign_packages.sh
+++ sign_packages.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+echo $PRIVATE_GPG_KEY > private.key
+
+cat private.key | wc -l
+
+gpg --allow-secret-key-import --import private.key
+
 wget https://grafanarel.s3.amazonaws.com/builds/grafana-2.0.0_beta3-1.x86_64.rpm
 
 cp rpmmacros ~/.rpmmacros
-
-echo $PRIVATE_GPG_KEY > private.key
-gpg --allow-secret-key-import --import private.key
 
 ./sign_expect *.rpm
