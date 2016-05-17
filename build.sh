ac43ef5585e26f9694806de7ce66086f9857c0e7
Torkel Ödegaard <torkel.odegaard@gmail.com>
2016-05-17 09:05:55 +0200

    reverted to build master

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ REPO_PATH=$GOPATH/src/github.com/grafana/grafana
 
 mkdir -p /go/src/github.com/grafana
 cd /go/src/github.com/grafana
-git clone --depth 1 https://github.com/grafana/grafana.git -b v3.0.x
+git clone --depth 1 https://github.com/grafana/grafana.git -b master
 
 # go get -u -v github.com/grafana/grafana
 
