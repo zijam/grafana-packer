dbe65230082a4f707fe4961715876be3eef14c30
bergquist <carl.bergquist@gmail.com>
2017-01-11 15:57:20 +0100

    add more logging

--- build.sh
+++ build.sh
@@ -10,7 +10,10 @@ REPO_PATH=$GOPATH/src/github.com/grafana/grafana
 mkdir -p /go/src/github.com/grafana
 cd /go/src/github.com/grafana
 
+echo "Grafana branch!!"
+echo $GRAFANA_BRANCH
 echo "GRAFANA BRANCH: ${GRAFANA_BRANCH}"
+
 git clone --depth 1 https://github.com/grafana/grafana.git -b $GRAFANA_BRANCH
 
 cd $REPO_PATH
